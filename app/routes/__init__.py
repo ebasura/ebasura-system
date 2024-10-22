565a19cd44acc112b1333a62b0626502eab1c2ce
bitress <cyannejustinl.vega12@gmail.com>
2024-10-23 05:46:14 +0800

    removed not needed functions

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,51 +4,15 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from ..routes.system_info import system_info_bp
 from ..routes.system_health import system_health_bp
-from ..engine import db
 
 def create_app():
     app = Flask(__name__)
-    UPLOAD_FOLDER = 'models'    
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
     CORS(app, resources={r"/*": {"origins": {"https://ebasura.online", "https://www.ebasura.online", "http://192.168.0.125:8000"}}})
 
     # Register blueprints
     app.register_blueprint(system_info_bp)
     app.register_blueprint(system_health_bp)
 
-    def generate_random_filename(filename):
-        """Generate a random filename with the same file extension."""
-        ext = os.path.splitext(filename)[1]  
-        return str(uuid.uuid4()) + ext 
-
-    @app.route('/upload-model', methods=['GET', 'POST'])
-    def upload():
-        if 'model_file' not in request.files:
-            return jsonify({"error": "No file part in the request"}), 400
-        
-        file = request.files['model_file']
-        description = request.form.get('model_description')
-        
-        # Validate the file and description
-        if file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
-        
-        if not description:
-            return jsonify({"error": "No model description provided"}), 400
-
-        # Save the file
-        random_filename = generate_random_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], random_filename)
-        file.save(file_path)
-
-        try:
-            query = "INSERT INTO models (description, file_path) VALUES (%s, %s)"
-            db.execute(query, (description, file_path))
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-
-        return jsonify({"message": "File uploaded successfully", "description": description}), 200
     
     @app.route('/')
     def ok():
